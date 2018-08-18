5523bf63400d6d7e16e0dfde53cabb34ca262bf4
J-VdS <38380578+J-VdS@users.noreply.github.com>
2018-08-18 14:29:42 +0200

    added indielmbot

---
+++ indielm_bot.py
@@ -0,0 +1,25 @@
+﻿import discord
+from discord.ext import commands
+
+with open('token.txt', 'r') as infile:
+    TOKEN = infile.readline()
+
+client = commands.Bot(command_prefix = '.%') #not used
+emos = {}
+
+@client.event
+async def on_ready():
+    print('Bot is ready')
+
+    #gets all custom emojis
+    for x in client.get_all_emojis():
+        emos[x.name] = x
+    print(emos)
+        
+@client.event
+async def on_message(message):
+    content = message.content
+    if 'router' in content or 'splitter' in content:
+        await client.add_reaction(message, emos['router'])
+    
+client.run(TOKEN)
