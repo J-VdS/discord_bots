d415780347d1e2bb3fd40e6ff691924b81587dcb
J-VdS <38380578+J-VdS@users.noreply.github.com>
2018-08-19 10:40:11 +0200

    !clear works
    !clear = deletes 100 messages in the current channel
    !clear <num> = deletes num messages in the current channel 
    -> !clear 5 = deletes 5 messages + !clear message

--- indielm_bot.py
+++ indielm_bot.py
@@ -1,10 +1,10 @@
-import discord
+﻿import discord
 from discord.ext import commands
 
 with open('token.txt', 'r') as infile:
     TOKEN = infile.readline()
 
-client = commands.Bot(command_prefix = '.%') #not used
+client = commands.Bot(command_prefix = '!') 
 emos = {}
 
 @client.event
@@ -21,9 +21,17 @@ async def on_message(message):
     content = message.content
     if 'router' in content or 'splitter' in content:
         await client.add_reaction(message, emos['router'])
+    print('rerout msg')
+    await client.process_commands(message)
 
 @client.event
 async def on_message_edit(before, after):
     await on_message(after)
    
+@client.command(pass_context=True)
+async def clear(ctx, limit=100):
+    channel = ctx.message.channel 
+    message = [msg async for msg in client.logs_from(channel, limit=int(limit)+1)]
+    await client.delete_messages(message)
+
 client.run(TOKEN)
